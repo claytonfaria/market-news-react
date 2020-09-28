bfca955dfad4e611b872f47c86e18576d4aa2270
Clayton Faria <claytonfaria@gmail.com>
2020-09-28 23:23:03 +0800

    update to env variable setup

--- src/API/fetchArticles.js
+++ src/API/fetchArticles.js
@@ -1,14 +1,6 @@
-let API_KEY;
-
-if (process.env.NODE_ENV !== 'production') {
-  API_KEY = process.env.REACT_APP_NYT_API_KEY;
-} else {
-  API_KEY = process.env.NYT_API_KEY;
-}
-
 const fetchArticles = async () => {
   const res = await fetch(
-    `https://api.nytimes.com/svc/topstories/v2/business.json?api-key=${API_KEY}`
+    `https://api.nytimes.com/svc/topstories/v2/business.json?api-key=${process.env.REACT_APP_NYT_API_KEY}`
   );
   const data = await res.json();
   return data.results;
