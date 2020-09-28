d754440375cbc154cf31efdf612abf041b55fe3a
Clayton Faria <claytonfaria@gmail.com>
2020-09-28 21:46:58 +0800

    netlify deployment preparations

--- src/API/fetchArticles.js
+++ src/API/fetchArticles.js
@@ -1,8 +1,15 @@
+let API_KEY;
+
+if (process.env.NODE_ENV !== 'production') {
+  API_KEY = process.env.REACT_APP_NYT_API_KEY;
+} else {
+  API_KEY = process.env.NYT_API_KEY;
+}
+
 const fetchArticles = async () => {
-  const res = await fetch('./data.json');
-  // const res = await fetch(
-  //   'https://api.nytimes.com/svc/topstories/v2/business.json?api-key=Isf67De2hk2oVH5t9rrXg9ceoNjWNaOq'
-  // );
+  const res = await fetch(
+    `https://api.nytimes.com/svc/topstories/v2/business.json?api-key=${API_KEY}`
+  );
   const data = await res.json();
   return data.results;
 };
